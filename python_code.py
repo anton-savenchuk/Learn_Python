8cc7e81e6e129217b4c4f22998c9c6bca8146c24
Anton Savenchuk <Anton.Savenchuk@gmail.com>
2022-05-30 14:23:32 +0700

    Написал функцию friends

--- python_code.py
+++ python_code.py
@@ -5,7 +5,34 @@
 # print()
 
 
+#######################################################################
+# Напишите функцию friends, которая из списка пар друзей сделает
+# словарь, в котором каждому человеку будет сопоставлено множество
+# его друзей.
 
+# friends([("Ivan", "Maria"), 
+#          ("Ella", "Ivan"), 
+#          ("Ivan", "Oleg")]) == \
+# {"Ivan":{"Maria", "Ella", "Oleg"},
+#  "Ella":{"Ivan"},
+#  "Maria": {"Ivan"},
+#  "Oleg": {"Ivan"}}
+
+names = [("Ivan", "Maria"),
+            ("Ella", "Ivan"),
+            ("Ivan", "Oleg")]
+
+def friends(pairs):
+    friends_dict = {}
+    for pair in pairs:
+        name, friend_name = pair
+        friends_dict[name] = set(friends_dict.get(name, set())) | {friend_name}
+        friends_dict[friend_name] = set(friends_dict.get(friend_name, set())) | {name}
+
+    return friends_dict
+
+
+print(friends(names))
 
 
 # print()
