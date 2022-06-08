ae89540fced522cba0a12f349546751020c42fab
Anton Savenchuk <Anton.Savenchuk@gmail.com>
2022-06-08 22:47:08 +0700

    Рещил задачу "Панграммы"

--- python_code.py
+++ python_code.py
@@ -2140,14 +2140,46 @@
 # False
 
 
-def is_magic(date: str) -> bool:
-    day, month, year = date.split('.')
+# def is_magic(date: str) -> bool:
+#     day, month, year = date.split('.')
 
-    return int(day) * int(month) == int(year[-2:])
+#     return int(day) * int(month) == int(year[-2:])
 
-print(is_magic('10.06.1960'))
-print(is_magic('11.06.1960'))
+# print(is_magic('10.06.1960'))
+# print(is_magic('11.06.1960'))
 
+
+####################################
+# Панграммы
+
+# Панграмма – это фраза, содержащая в себе все буквы алфавита.
+# Обычно панграммы используют для презентации шрифтов, чтобы можно
+# было в одной фразе рассмотреть все глифы.
+
+# Напишите функцию, is_pangram(text) которая принимает в качестве
+# аргумента строку текста на английском языке и возвращает значение
+# True если текст является панграммой и False в противном случае.
+
+# Примечание 1. Гарантируется, что введенная строка содержит только
+# буквы английского алфавита.
+
+# Примечание 2. Следующий программный код:
+# print(is_pangram('Jackdaws love my big sphinx of quartz'))
+# print(is_pangram('The jay pig fox zebra and my wolves quack'))
+# print(is_pangram('Hello world'))
+
+# должен выводить:
+# True
+# True
+# False
+
+
+def is_pangram(text: str) -> bool:
+    return len(set(''.join(text.lower().split()))) == 26
+
+print(is_pangram('Jackdaws love my big sphinx of quartz'))
+print(is_pangram('The jay pig fox zebra and my wolves quack'))
+print(is_pangram('The quick brown fox jumps over the lazy dog'))
 
 
 # print()
