b5f4aad94397ca79655d00637bc1dbc55c26d0c6
Anton Savenchuk <Anton.Savenchuk@gmail.com>
2022-07-20 15:27:08 +0700

    Решил задачу "Декоратор sum"

--- python_code.py
+++ python_code.py
@@ -4164,49 +4164,91 @@
 
 # Sample Output:
 # dekoratory-eto-kruto-
+# from functools import wraps
+
+
+# t = {'ё': 'yo', 'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ж': 'zh',
+#      'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p',
+#      'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh',
+#      'щ': 'shch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
+
+
+# def get_separator_decorator(chars=" !?"):
+#     """Pass arguments to decorator."""
+
+#     def get_separator(func):
+#         """Decarator.
+
+#         Replace the repeating separator with one.
+#         """
+
+#         @wraps(func)
+#         def _wrapper(*args):
+#             _str = func(*args)
+
+#             for i in chars:
+#                 if i in _str:
+#                     _str = _str.replace(i, "-")
+
+#             while "--" in _str:
+#                 _str = _str.replace("--", "-")
+
+#             return _str
+
+#         return _wrapper
+#     return get_separator
+
+
+# @get_separator_decorator("?!:;,. ")
+# def get_translit(string: str) -> str:
+#     """Convert a Cyrillic string to Latin."""
+#     return "".join(t[i] if i in t else i for i in string.lower())
+
+
+# s = input()
+# print(get_translit(s))
+
+
+####################################
+# Объявите функцию с именем get_list и следующим описанием в теле
+# функции:
+
+# '''Функция для формирования списка целых значений'''
+
+# Сама функция должна формировать и возвращать список целых чисел,
+# который поступает на ее вход в виде строки из целых чисел, записанных
+# через пробел.
+
+# Определите декоратор, который выполняет суммирование значений из
+# списка этой функции и возвращает результат.
+# Внутри декоратора декорируйте переданную функцию get_list с помощью
+# команды @wraps (не забудьте сделать импорт: from functools import
+# wraps). Такое декорирование необходимо, чтобы исходная функция
+# get_list сохраняла свои локальные свойства: __name__ и __doc__.
+
+# Примените декоратор к функции get_list, но не вызывайте ее.
 from functools import wraps
 
 
-t = {'ё': 'yo', 'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'д': 'd', 'е': 'e', 'ж': 'zh',
-     'з': 'z', 'и': 'i', 'й': 'y', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p',
-     'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'h', 'ц': 'c', 'ч': 'ch', 'ш': 'sh',
-     'щ': 'shch', 'ъ': '', 'ы': 'y', 'ь': '', 'э': 'e', 'ю': 'yu', 'я': 'ya'}
+def get_sum(func):
+    """Decorator.
+
+    Return the sum of the elements of the given iterable.
+    """
+
+    @wraps(func)
+    def _wrapper(*args, **kwargs):
+        return sum(func(*args, **kwargs))
+
+    return _wrapper
 
 
-def get_separator_decorator(chars=" !?"):
-    """Pass arguments to decorator."""
-
-    def get_separator(func):
-        """Decarator.
-
-        Replace the repeating separator with one.
-        """
-
-        @wraps(func)
-        def _wrapper(*args):
-            _str = func(*args)
-
-            for i in chars:
-                if i in _str:
-                    _str = _str.replace(i, "-")
-
-            while "--" in _str:
-                _str = _str.replace("--", "-")
-
-            return _str
-
-        return _wrapper
-    return get_separator
+@get_sum
+def get_list(lst_in: str) -> list:
+    """Функция для формирования списка целых значений."""
+    return [int(i) for i in lst_in.split()]
 
 
-@get_separator_decorator("?!:;,. ")
-def get_translit(string: str) -> str:
-    """Convert a Cyrillic string to Latin."""
-    return "".join(t[i] if i in t else i for i in string.lower())
-
-
-s = input()
-print(get_translit(s))
 
 
 # print()
