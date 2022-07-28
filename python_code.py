8e180a8273fa7e9a7bc9675df252b5bfce802113
Anton Savenchuk <Anton.Savenchuk@gmail.com>
2022-07-28 09:07:31 +0700

    Решил задачу "Средняя оценка по предметам"

--- python_code.py
+++ python_code.py
@@ -4258,7 +4258,7 @@
 # В качестве ответа укажите найденные два числа через запятую без других
 # знаков и пробелов. Сперва количество, потом сумма
 
-# with open("numbers.txt", "r", encoding="utf-8") as file:
+# with open("text_files/numbers.txt", "r", encoding="utf-8") as file:
 #     _cnt, _sum = 0, 0
 #     while (num := file.readline().strip()):
 #         if len(num) == 3:
@@ -4272,7 +4272,7 @@
 ####################################
 # Восстановление исходной строки
 
-# with open("dataset_3363_2.txt", "r", encoding="utf-8") as file:
+# with open("text_files/dataset_3363_2.txt", "r", encoding="utf-8") as file:
 #     _file = file.readline().strip()
 
 #     string_new = ""
@@ -4287,7 +4287,7 @@
 #         string_new += _file[cnt] * int(_file[cnt + 1:lenght])
 #         cnt = lenght
 
-#     with open("dataset_3363_2_encode.txt", "w", encoding="utf-8") as file:
+#     with open("text_files/dataset_3363_2_encode.txt", "w", encoding="utf-8") as file:
 #         _file = file.write(string_new)
 
 
@@ -4314,21 +4314,84 @@
 # Sample Output:
 # abc 3
 
-with open("dataset_3363_3.txt") as file_in:
-    _file = file_in.read().lower().strip().split()
-    _file.sort()
+# with open("text_files/dataset_3363_3.txt") as file_in:
+#     _file = file_in.read().lower().strip().split()
+#     _file.sort()
 
-    max_cnt = 0
-    for _word in _file:
-        cnt = _file.count(_word)
-        if cnt > max_cnt:
-            max_cnt = cnt
-            result_word = _word
+#     max_cnt = 0
+#     for _word in _file:
+#         cnt = _file.count(_word)
+#         if cnt > max_cnt:
+#             max_cnt = cnt
+#             result_word = _word
 
-with open("often_used_word.txt", "w") as file_out:
-    file_out.write(f"{result_word} {str(max_cnt)}")
+# with open("text_files/often_used_word.txt", "w") as file_out:
+#     file_out.write(f"{result_word} {str(max_cnt)}")
 
 
+####################################
+# Имеется файл с данными по успеваемости абитуриентов. Он представляет
+# из себя набор строк, где в каждой строке записана следующая
+# информация:
+
+# Фамилия;Оценка_по_математике;Оценка_по_физике;Оценка_по_русскому_языку
+
+# Поля внутри строки разделены точкой с запятой, оценки — целые числа.
+
+# Напишите программу, которая считывает исходный файл с подобной
+# структурой и для каждого абитуриента записывает его среднюю оценку по
+# трём предметам на отдельной строке, соответствующей этому абитуриенту,
+# в файл с ответом.
+
+# Также вычислите средние баллы по математике, физике и русскому языку
+# по всем абитуриентам и добавьте полученные значения, разделённые
+# пробелом, последней строкой в файл с ответом.
+
+# В качестве ответа на задание прикрепите полученный файл со средними
+# оценками по каждому ученику и одной строкой со средними оценками по
+# трём предметам.
+
+# Примечание. Для разбиения строки на части по символу ';' можно
+# использовать метод split следующим образом:
+
+# print('First;Second-1 Second-2;Third'.split(';'))
+# # ['First', 'Second-1 Second-2', 'Third']
+
+# Sample Input:
+# Петров;85;92;78
+# Сидоров;100;88;94
+# Иванов;58;72;85
+
+# Sample Output:
+# 85.0
+# 94.0
+# 71.666666667
+# 81.0 84.0 85.666666667
+
+with open("text_files/dataset_3363_4.txt") as file_in, open(
+    "text_files/dataset_3363_4_answer.txt", "a", encoding="utf-8"
+) as file_out:
+
+    total_rating_cntr, students_cntr = [], 0
+    for row in file_in:
+
+        row = [int(i) if i.isdigit() else i for i in row.strip().split(";")]
+        rating_cntr, courses_cntr = 0, len(row) - 1
+
+        for i in range(1, len(row)):
+            rating_cntr += row[i]
+
+            if len(total_rating_cntr) != courses_cntr:
+                total_rating_cntr.append(row[i])
+            else:
+                total_rating_cntr[i - 1] += row[i]
+
+        students_cntr += 1
+        file_out.write(f"{rating_cntr / courses_cntr}\n")
+
+    file_out.write(
+        " ".join([str(num / students_cntr) for num in total_rating_cntr])
+    )
 
 
 
