756a73cf48ac47e750e45beba0101f2849eab042
Anton Savenchuk <Anton.Savenchuk@gmail.com>
2022-08-05 17:22:15 +0700

    Решил задачу "Пароль длиной N"

--- python_code.py
+++ python_code.py
@@ -4634,23 +4634,79 @@
 # 1 1 1 3 5 9 17
 
 
-def get_balakirev(n: int) -> int:
-    """Determine the first N numbers of the Balakirev sequence."""
-    _1 = _2 = _3 = 1
-    for i in range(n):
-        if i in (0, 1, 2):
-            yield 1
-        else:
-            _sum = _1 + _2 + _3
-            _1, _2, _3 = _sum, _1, _2
-            yield _sum
+# def get_balakirev(n: int) -> int:
+#     """Determine the first N numbers of the Balakirev sequence."""
+#     _1 = _2 = _3 = 1
+#     for i in range(n):
+#         if i in (0, 1, 2):
+#             yield 1
+#         else:
+#             _sum = _1 + _2 + _3
+#             _1, _2, _3 = _sum, _1, _2
+#             yield _sum
 
 
-n = int(input())
-seq = get_balakirev(n)
+# n = int(input())
+# seq = get_balakirev(n)
 
-for i in seq:
-    print(i, end=" ")
+# for i in seq:
+#     print(i, end=" ")
+
+
+# ####################################
+# Вводится натуральное число N (N > 8). Необходимо определить
+# функцию-генератор, которая бы выдавала пароль длиной N символов из
+# случайных букв, цифр и некоторых других знаков. Для получения
+# последовательности допустимых символов для генерации паролей в
+# программе импортированы две строки: ascii_lowercase, ascii_uppercase
+# (см. листинг ниже), на основе которых формируется общий список:
+
+# from string import ascii_lowercase, ascii_uppercase
+# chars = ascii_lowercase + ascii_uppercase + "0123456789!?@#$*"
+
+# Функция-генератор должна при каждом вызове возвращать новый пароль из
+# случайно выбранных символов chars длиной N и делать это бесконечно,
+# то есть, вызывать ее можно бесконечное число раз. Сгенерировать
+# случайный индекс indx в диапазоне [a; b] для символа можно с помощью
+# функции randint модуля random:
+
+# import random
+# random.seed(1)
+# indx = random.randint(a, b)
+
+# Сгенерируйте с помощью этой функции первые пять паролей и выведите их
+# в столбик (каждый с новой строки).
+
+# Sample Input:
+# 10
+
+# Sample Output:
+# riGp?58WAm
+# !dX3a5IDnO
+# dcdbWB2d*C
+# 4?DSDC6Lc1
+# mxLpQ@2@yM
+
+import random
+from string import ascii_lowercase, ascii_uppercase
+
+# установка зерна датчика случайных чисел (не менять)
+random.seed(1)
+
+# здесь продолжайте программу
+chars = ascii_lowercase + ascii_uppercase + "0123456789!?@#$*"
+lenght = int(input())
+
+
+def get_password(n: int) -> str:
+    """Generate a password."""
+    for _ in range(n):
+        indx = random.randint(0, len(chars) - 1)
+        yield chars[indx]
+
+
+for _ in range(5):
+    print("".join(get_password(lenght)))
 
 
 
