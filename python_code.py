f26ea05bb1e5b125e252f66115a0dfadda90f6cf
Anton Savenchuk <Anton.Savenchuk@gmail.com>
2022-09-29 15:06:05 +0700

    Решил задачу "Таблица умножения"

--- python_code.py
+++ python_code.py
@@ -5745,25 +5745,82 @@
 # Левая четверть: 0
 
 
-n = int(input())
-matrix = [list(map(int, input().split())) for _ in range(n)]
-top = bottom = left = right = 0
+# n = int(input())
+# matrix = [list(map(int, input().split())) for _ in range(n)]
+# top = bottom = left = right = 0
 
-for i in range(n):
-    for j in range(n):
-        if (j < i < n - 1 - j):
-            left += matrix[i][j]
-        elif (n - 1 - j < i < j):
-            right += matrix[i][j]
-        elif (j > i < n - 1 - j):
-            top += matrix[i][j]
-        elif (n - 1 - j < i > j):
-            bottom += matrix[i][j]
+# for i in range(n):
+#     for j in range(n):
+#         if (j < i < n - 1 - j):
+#             left += matrix[i][j]
+#         elif (n - 1 - j < i < j):
+#             right += matrix[i][j]
+#         elif (j > i < n - 1 - j):
+#             top += matrix[i][j]
+#         elif (n - 1 - j < i > j):
+#             bottom += matrix[i][j]
 
-print(f"Верхняя четверть: {top}")
-print(f"Правая четверть: {right}")
-print(f"Нижняя четверть: {bottom}")
-print(f"Левая четверть: {left}")
+# print(f"Верхняя четверть: {top}")
+# print(f"Правая четверть: {right}")
+# print(f"Нижняя четверть: {bottom}")
+# print(f"Левая четверть: {left}")
+
+
+# ####################################
+# Таблица умножения
+
+# На вход программе подаются два натуральных числа n и m — количество
+# строк и столбцов в матрице. Создайте матрицу mult размером n×m и
+# заполните её таблицей умножения по формуле mult[i][j] = i * j.
+
+# Формат входных данных
+# На вход программе на разных строках подаются два числа n и m —
+# количество строк и столбцов в матрице.
+
+# Формат выходных данных
+# Программа должна вывести таблицу умножения отводя на вывод каждого
+# числа ровно 3 символа (для этого используйте строковый метод ljust()).
+
+# Sample Input 1:
+# 4
+# 6
+
+# Sample Output 1:
+# 0  0  0  0  0  0
+# 0  1  2  3  4  5
+# 0  2  4  6  8  10
+# 0  3  6  9  12 15
+
+# Sample Input 2:
+# 3
+# 5
+
+# Sample Output 2:
+# 0  0  0  0  0
+# 0  1  2  3  4
+# 0  2  4  6  8
+
+# Sample Input 3:
+# 6
+# 6
+
+# Sample Output 3:
+# 0  0  0  0  0  0
+# 0  1  2  3  4  5
+# 0  2  4  6  8  10
+# 0  3  6  9  12 15
+# 0  4  8  12 16 20
+# 0  5  10 15 20 25
+
+
+n, m = int(input()), int(input())
+
+mult = [[i * j for j in range(m)] for i in range(n)]
+
+for row in mult:
+    for i in row:
+        print(str(i).ljust(3), end="")
+    print()
 
 
 # print()
