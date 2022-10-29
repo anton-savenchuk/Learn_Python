5ad0b858b7135a7d1aa6a77ebb1fd28cb799a733
Anton Savenchuk <Anton.Savenchuk@gmail.com>
2022-10-29 12:31:50 +0700

    Решил задачу "Большой подвиг 10"

--- python_code.py
+++ python_code.py
@@ -6239,47 +6239,153 @@
 # Вывод в формате: идти ехать ходить
 
 
-class Translator:
-    def __init__(self):
-        self.translate_dict = {}
+# class Translator:
+#     def __init__(self):
+#         self.translate_dict = {}
 
-    def add(self, eng, rus):
-        """Add a new bundle of English and Russian word."""
-        if rus not in self.translate_dict.get(eng, []):
-            self.translate_dict[eng] = self.translate_dict.get(eng, []) + [rus]
+#     def add(self, eng, rus):
+#         """Add a new bundle of English and Russian word."""
+#         if rus not in self.translate_dict.get(eng, []):
+#             self.translate_dict[eng] = self.translate_dict.get(eng, []) + [rus]
 
-    def remove(self, eng):
-        """Delete a link in an english word."""
-        self.translate_dict.pop(eng, False)
+#     def remove(self, eng):
+#         """Delete a link in an english word."""
+#         self.translate_dict.pop(eng, False)
 
-    def translate(self, eng):
-        """Translate from English into Russian.
+#     def translate(self, eng):
+#         """Translate from English into Russian.
 
-        Return a list of Russian words.
-        """
-        return self.translate_dict[eng]
+#         Return a list of Russian words.
+#         """
+#         return self.translate_dict[eng]
 
 
-eng_rus = [
-    ["tree", "дерево"],
-    ["tree", "дерево"],
-    ["car", "машина"],
-    ["car", "автомобиль"],
-    ["leaf", "лист"],
-    ["river", "река"],
-    ["go", "идти"],
-    ["go", "ехать"],
-    ["go", "ходить"],
-    ["milk", "молоко"],
-]
+# eng_rus = [
+#     ["tree", "дерево"],
+#     ["tree", "дерево"],
+#     ["car", "машина"],
+#     ["car", "автомобиль"],
+#     ["leaf", "лист"],
+#     ["river", "река"],
+#     ["go", "идти"],
+#     ["go", "ехать"],
+#     ["go", "ходить"],
+#     ["milk", "молоко"],
+# ]
 
-tr = Translator()
+# tr = Translator()
 
-for i in range(len(eng_rus)):
-    tr.add(eng_rus[i][0], eng_rus[i][1])
+# for i in range(len(eng_rus)):
+#     tr.add(eng_rus[i][0], eng_rus[i][1])
 
-tr.remove("car")
-print(*tr.translate("go"))
+# tr.remove("car")
+# print(*tr.translate("go"))
+
+
+# ####################################
+# Большой подвиг 10. Объявите два класса:
+
+# Cell - для представления клетки игрового поля;
+# GamePole - для управления игровым полем, размером N x N клеток.
+
+# С помощью класса Cell предполагается создавать отдельные клетки командой:
+
+# c1 = Cell(around_mines, mine)
+
+# Здесь around_mines - число мин вокруг данной клетки поля; mine - булева
+# величина (True/False), означающая наличие мины в текущей клетке. При
+# этом, в каждом объекте класса Cell должны создаваться локальные
+# свойства:
+
+# around_mines - число мин вокруг клетки (начальное значение 0);
+# mine - наличие мины в текущей клетке (True/False);
+# fl_open - открыта/закрыта клетка - булево значение (True/False).
+# Изначально все клетки закрыты (False).
+
+# С помощью класса GamePole должна быть возможность создавать квадратное
+# игровое поле с числом клеток N x N:
+
+# pole_game = GamePole(N, M)
+
+# Здесь N - размер поля; M - общее число мин на поле. При этом, каждая
+# клетка представляется объектом класса Cell и все объекты хранятся в
+# двумерном списке N x N элементов - локальном свойстве pole объекта
+# класса GamePole.
+
+# В классе GamePole должны быть также реализованы следующие методы:
+
+# init() - инициализация поля с новой расстановкой M мин (случайным
+# образом по игровому полю, разумеется каждая мина должна находиться в
+# отдельной клетке).
+# show() - отображение поля в консоли в виде таблицы чисел открытых
+# клеток (если клетка не открыта, то отображается символ #).
+
+# При создании экземпляра класса GamePole в его инициализаторе следует
+# вызывать метод init() для первоначальной инициализации игрового поля.
+
+# В классе GamePole могут быть и другие вспомогательные методы.
+
+# Создайте экземпляр pole_game класса GamePole с размером поля N = 10 и
+# числом мин M = 12.
+
+# P.S. На экран в программе ничего выводить не нужно.
+from random import randint
+
+
+class Cell:
+    def __init__(self, around_mines: int = 0, mine: bool = False):
+        self.around_mines = around_mines
+        self.mine = mine
+        self.fl_open: bool = False
+
+
+class GamePole:
+    def __init__(self, N: int, M: int):
+        self.N = N
+        self.M = M
+        self.pole = [[Cell() for _ in range(self.N)] for _ in range(self.N)]
+        self.init()
+
+    def init(self):
+        while self.M:
+            i = randint(0, self.N - 1)
+            j = randint(0, self.N - 1)
+
+            if self.pole[i][j].mine:
+                continue
+
+            self.pole[i][j].mine = True
+            self.M -= 1
+
+        indx = (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)
+
+        for x in range(self.N):
+            for y in range(self.N):
+                if not self.pole[x][y].mine:
+                    mines = sum(
+                        (
+                            self.pole[x + i][y + j].mine
+                            for i, j in indx
+                            if 0 <= x + i < self.N and 0 <= y + j < self.N
+                        )
+                    )
+                    self.pole[x][y].around_mines = mines
+
+    def show(self):
+        for row in self.pole:
+            print(
+                *map(
+                    lambda x: "#"
+                    if not x.fl_open
+                    else x.around_mines
+                    if not x.mine
+                    else "*",
+                    row,
+                )
+            )
+
+
+pole_game = GamePole(10, 12)
 
 
 # print()
